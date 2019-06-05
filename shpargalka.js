312fda23dbbea1efe8d83b6d6aacc2175c887e8f
BatofSky <51396283+BatofSky@users.noreply.github.com>
2019-06-05 16:17:20 +0900

    Add files via upload

---
+++ shpargalka.js
@@ -0,0 +1,103 @@
+	let num = 100; //переменная- число
+let text ="text"; //переменная-текст
+let object = { //переменная - объект
+	key1:"что-то" ,//ключ со значением
+	key2:"что-то еще"
+onclick = function(){} // клики(нажатие) на различные html элементы
+} 
+style.height = "100px"; //меняет различные css свойства элементов
+document.querySelector() // находит нужный элемент в html по классу 
+alert() //всплывающее окно на странице
+
+
+
+
+<div class="question2" style="display: none;"><!--Третий-->
+	<div class="col-12 border shadow-lg p-3 mb-5 rounded">
+		<h2 class="text-uppercase text-center">453+124+241=?</h2>
+	</div>
+	<div class="col-9 border shadow-lg p-3 mb-3 bg-dark rounded bg-dark container otvet7" style="margin-top: 140px">
+		<h5 class="text-white text-center">818</h5>
+	</div>
+	<div class="col-9 border shadow-lg p-3 mb-3 bg-dark rounded bg-dark container otvet8">
+		<h5 class="text-white text-center">820</h5>
+	</div>
+	<div class="col-9 border shadow-lg p-3 mb-5 bg-dark rounded bg-dark container otvet9">
+		<h5 class="text-white text-center">798</h5>
+	</div>
+</div>
+<div class="question3" style="display: none;"><!--Четвёртый-->
+	<div class="col-12 border shadow-lg p-3 mb-5 rounded">
+		<h2 class="text-uppercase text-center">Сколько лет Губке Бобу?</h2>
+	</div>
+	<div class="col-9 border shadow-lg p-3 mb-3 bg-dark rounded bg-dark container otvet10" style="margin-top: 140px">
+		<h5 class="text-white text-center">32</h5>
+	</div>
+	<div class="col-9 border shadow-lg p-3 mb-3 bg-dark rounded bg-dark container otvet11">
+		<h5 class="text-white text-center">18</h5>
+	</div>
+	<div class="col-9 border shadow-lg p-3 mb-5 bg-dark rounded bg-dark container otvet12">
+		<h5 class="text-white text-center">23</h5>
+	</div>
+</div>
+<div class="question4" style="display: none;"><!--Пятый-->
+	<div class="col-12 border shadow-lg p-3 mb-5 rounded">
+		<h2 class="text-uppercase text-center">Кто изображен на картинке?</h2>
+	</div>
+	<div class="col-2 container mx-auto">
+		<img src="Вопрос.jpg">
+	</div>
+	<div class="col-9 border shadow-lg p-3 mb-3 bg-dark rounded bg-dark container otvet13" style="margin-top: 140px">
+		<h5 class="text-white text-center">Гошан</h5>
+	</div>
+	<div class="col-9 border shadow-lg p-3 mb-3 bg-dark rounded bg-dark container otvet14">
+		<h5 class="text-white text-center">Геннадий</h5>
+	</div>
+	<div class="col-9 border shadow-lg p-3 mb-5 bg-dark rounded bg-dark container otvet15">
+		<h5 class="text-white text-center">Георгий</h5>
+	</div>
+</div>
+<div class="question5" style="display: none;"><!--Шестой-->
+	<div class="col-12 border shadow-lg p-3 mb-5 rounded">
+		<h2 class="text-uppercase text-center">Из какой игры голос?</h2>
+	</div>
+	<div class="col-3 container mx-auto">
+		<audio controls>
+    	<source src="audio/music.ogg" type="audio/ogg; codecs=vorbis">
+    	<source src="SOLDAT.mp3" type="audio/mpeg">
+    Тег audio не поддерживается вашим браузером. 
+    	<a href="audio/music.mp3">Скачайте музыку</a>.
+  		</audio>
+	</div>
+	<div class="col-9 border shadow-lg p-3 mb-3 bg-dark rounded bg-dark container otvet16" style="margin-top: 140px">
+		<h5 class="text-white text-center">Dota 2</h5>
+	</div>
+	<div class="col-9 border shadow-lg p-3 mb-3 bg-dark rounded bg-dark container otvet17">
+		<h5 class="text-white text-center">Team Fortress 2</h5>
+	</div>
+	<div class="col-9 border shadow-lg p-3 mb-5 bg-dark rounded bg-dark container otvet18">
+		<h5 class="text-white text-center">PUBG</h5>
+	</div>
+</div>
+<div class="question6" style="display: none;"><!--Седьмой-->
+	<div class="col-12 border shadow-lg p-3 mb-5 rounded">
+		<h2 class="text-uppercase text-center">Кто это?</h2>
+	</div>
+	<div class="col-5 container mx-auto">
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/zW9Q7FU1mnk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+	</div>
+	<div class="col-9 border shadow-lg p-3 mb-3 bg-dark rounded bg-dark container otvet19" style="margin-top: 140px">
+		<h5 class="text-white text-center">Кролик Роджер</h5>
+	</div>
+	<div class="col-9 border shadow-lg p-3 mb-3 bg-dark rounded bg-dark container otvet20">
+		<h5 class="text-white text-center">Бакс Банни</h5>
+	</div>
+	<div class="col-9 border shadow-lg p-3 mb-5 bg-dark rounded bg-dark container otvet21">
+		<h5 class="text-white text-center">Какая-то жирная крыса</h5>
+	</div>
+</div>
+<div class="question7" style="display: none;"><!--Седьмой-->
+	<div style="height: 250px"></div>
+	<div class="col-12 border shadow-lg p-3 mb-5 rounded">
+		<h2 class="text-uppercase text-center">Ты справился, умничка :*</h2>
+	</div>
